3a00cccc844c5d603d97301c8332964cbe130d16
tushargupta <gupta.t@husky.neu.edu>
2016-12-07 00:11:41 -0500

    Stopwords implemented in Index

--- task1_indexer_stopwords.py
+++ task1_indexer_stopwords.py
@@ -8,8 +8,15 @@ import operator
 
 inverted_dict_unigram = {}
 
+stopwords = []
+
+def make_stopwords_list():
+	filename = open("common_words" , "r")
+	for i in filename:
+		stopwords.append(i.strip('\n'))
+
+
 def make_dict_unigram(file):
-	print file
 	filename = open(file,'r')
 	current_file = []
 	for i in filename:
@@ -19,7 +26,7 @@ def make_dict_unigram(file):
 
 	for i in xrange(len(current_file)):
 		token = current_file[i].strip('\n')
-		if token != '' or token != ' ':
+		if (token != '' or token != ' ') and token not in stopwords:
 			if token in inverted_dict_unigram:
 				a = inverted_dict_unigram[token]
 				if docid in a:
@@ -35,6 +42,7 @@ if __name__ == "__main__":
 	start_time = time.time()
 	i_n = 0
 	i = 1
+	make_stopwords_list()
 	for filename in glob.glob('Tokenized_Corpus/*.txt'):
 		make_dict_unigram(filename)
 		i_n += 0.0312
@@ -48,7 +56,7 @@ if __name__ == "__main__":
 
 	sorted_keys = sorted(inverted_dict_unigram.items(), key=operator.itemgetter(0), reverse=False)
 
-	mydict1 = open('Inverted_Index.txt','w')
+	mydict1 = open('Inverted_Index_Stopped.txt','w')
 	for i in sorted_keys:
 		sorted_doc = sorted(i[1].items(), key=operator.itemgetter(0), reverse=False)
 		mydict1.writelines(i[0]+' -> ')
